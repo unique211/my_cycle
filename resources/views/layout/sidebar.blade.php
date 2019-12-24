13086d208de89862844df04b6b0149848f5fb021
Sagar Morvadiya <morvadiyasagar99@gmail.com>
2019-12-24 11:35:03 +0530

    user accesswise sidebar maintain

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -70,7 +70,7 @@
                             <i class="fa fa-plus fa-lg"></i>
                         </span>
                         <span class="text">
-                              @lang('site_lables.User_Access')
+                            @lang('site_lables.User_Access')
                         </span>
                         <span class="menu-hover"></span>
                     </a>
@@ -84,7 +84,7 @@
                             <i class="fa fa-plus fa-lg"></i>
                         </span>
                         <span class="text">
-                             @lang('site_lables.Instructor')
+                            @lang('site_lables.Instructor')
                         </span>
                         <span class="menu-hover"></span>
                     </a>
@@ -114,7 +114,7 @@
                             <i class="fa fa-plus fa-lg"></i>
                         </span>
                         <span class="text">
-                             @lang('site_lables.Package')
+                            @lang('site_lables.Package')
                         </span>
                         <span class="menu-hover"></span>
                     </a>
@@ -129,7 +129,7 @@
                             <i class="fa fa-plus fa-lg"></i>
                         </span>
                         <span class="text">
-                             @lang('site_lables.Class_Category')
+                            @lang('site_lables.Class_Category')
                         </span>
                         <span class="menu-hover"></span>
                     </a>
@@ -144,7 +144,7 @@
                             <i class="fa fa-plus fa-lg"></i>
                         </span>
                         <span class="text">
-                             @lang('site_lables.Room')
+                            @lang('site_lables.Room')
                         </span>
                         <span class="menu-hover"></span>
                     </a>
@@ -159,7 +159,7 @@
                             <i class="fa fa-plus fa-lg"></i>
                         </span>
                         <span class="text">
-                             @lang('site_lables.Class')
+                            @lang('site_lables.Class')
                         </span>
                         <span class="menu-hover"></span>
                     </a>
@@ -174,7 +174,7 @@
                             <i class="fa fa-plus fa-lg"></i>
                         </span>
                         <span class="text">
-                             @lang('site_lables.Class_Schedule')
+                            @lang('site_lables.Class_Schedule')
                         </span>
                         <span class="menu-hover"></span>
                     </a>
@@ -189,7 +189,7 @@
                             <i class="fa fa-plus fa-lg"></i>
                         </span>
                         <span class="text">
-                             @lang('site_lables.Deals')
+                            @lang('site_lables.Deals')
                         </span>
                         <span class="menu-hover"></span>
                     </a>
@@ -219,40 +219,68 @@
                             <i class="fa fa-plus fa-lg"></i>
                         </span>
                         <span class="text">
-                             @lang('site_lables.Member_Attendence_Tacking')
+                            @lang('site_lables.Member_Attendence_Tacking')
                         </span>
                         <span class="menu-hover"></span>
                     </a>
                 </li>
 
                 @endif
-                @if($val->menuid==13)
+                @if($val->submenuid==1 || $val->submenuid==2
+                ||$val->submenuid==3||$val->submenuid==4||$val->submenuid==5)
+                <?php $setting=1; ?>
 
+
+                @endif
+                @if($val->submenuid==6 || $val->submenuid==7)
+                <?php $report=1; ?>
+
+                @endif
+                @endforeach
+                @if($setting==1)
                 <li class="openable">
                     <a href="#">
                         <span class="menu-icon">
                             <i class="fa fa-cog"></i>
                         </span>
                         <span class="text">
-                             @lang('site_lables.Setting')
+                            @lang('site_lables.Setting')
                         </span>
                         <span class="menu-hover"></span>
 
                     </a>
                     <ul class="submenu">
-                        <li><a href="{{ url('language') }}"><span class="submenu-label"> <i class="fa fa-plus fa-lg"></i>  @lang('site_lables.Language')</span></a></li>
-                        <li><a href="{{ url('member_type') }}"><span class="submenu-label"> <i class="fa fa-plus fa-lg"></i> @lang('site_lables.Member_Type')</span></a></li>
-                        <li><a href="{{ url('site_setting') }}"><span class="submenu-label"> <i class="fa fa-plus fa-lg"></i> @lang('site_lables.Site_Setting')</span></a></li>
-                        <li><a href="{{ url('mobile_notification') }}"><span class="submenu-label"> <i class="fa fa-plus fa-lg"></i> @lang('site_lables.Mobile_Notification')</span></a></li>
+                        @foreach($sidebar as $val)
+                        @if($val->submenuid==1)
+                        <li><a href="{{ url('language') }}"><span class="submenu-label"> <i
+                                        class="fa fa-plus fa-lg"></i> @lang('site_lables.Language')</span></a></li>
+                        @endif
+                        @if($val->submenuid==2)
+                        <li><a href="{{ url('member_type') }}"><span class="submenu-label"> <i
+                                        class="fa fa-plus fa-lg"></i> @lang('site_lables.Member_Type')</span></a></li>
+                        @endif
+                        @if($val->submenuid==3)
+                        <li><a href="{{ url('site_setting') }}"><span class="submenu-label"> <i
+                                        class="fa fa-plus fa-lg"></i> @lang('site_lables.Site_Setting')</span></a></li>
+                        @endif
+                        @if($val->submenuid==4)
+                        <li><a href="{{ url('mobile_notification') }}"><span class="submenu-label"> <i
+                                        class="fa fa-plus fa-lg"></i>
+                                    @lang('site_lables.Mobile_Notification')</span></a></li>
 
-                        <li><a href="{{ url('profile_manager') }}"><span class="submenu-label"><i class="fa fa-plus fa-lg"></i> @lang('site_lables.Profile_Manager')</span></a></li>
+                        @endif
+                        @if($val->submenuid==5) <li><a href="{{ url('profile_manager') }}"><span
+                                    class="submenu-label"><i class="fa fa-plus fa-lg"></i>
+                                    @lang('site_lables.Profile_Manager')</span></a>
+                        </li>
+                        @endif
+                        @endforeach
                     </ul>
                 </li>
-
                 @endif
-                @if($val->menuid==14)
+                @if($report==1)
+                <li class="openable">
 
-                 <li class="openable">
                     <a href="#">
                         <span class="menu-icon">
                             <i class="fa fa-file fa-lg"></i>
@@ -264,16 +292,21 @@
 
                     </a>
                     <ul class="submenu">
-                        {{-- <li><a href="{{ url('class_booking') }}"><span class="submenu-label"><i class="fa fa-plus fa-lg"></i> @lang('site_lables.Class_Booking')</span></a>
-                        </li> --}}
-                        <li><a href="{{ url('attendence_rating') }}"><span class="submenu-label"><i class="fa fa-plus fa-lg"></i>  @lang('site_lables.Attendence_&_Rating')</span></a></li>
-
-                    </ul>
-                </li>
+                        @foreach($sidebar as $val)
+                        @if($val->submenuid==6)
+                        {{-- <li><a href="{{ url('class_booking') }}"><span class="submenu-label"><i
+                                class="fa fa-plus fa-lg"></i> @lang('site_lables.Class_Booking')</span></a>
+                </li> --}}
                 @endif
-                @endforeach
-                @endif
-
+                @if($val->submenuid==7)
+                <li><a href="{{ url('attendence_rating') }}"><span class="submenu-label"><i
+                                class="fa fa-plus fa-lg"></i> @lang('site_lables.Attendence_&_Rating')</span></a></li>
+                                @endif
+                                @endforeach
+            </ul>
+            </li>
+            @endif
+            @endif
             </ul>
 
             {{--  <div class="alert alert-info">
